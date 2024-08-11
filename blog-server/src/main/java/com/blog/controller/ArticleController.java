1e9c29a78a2df6e82efb1e4ccb64d2d0ea2c387e
wolawola <3513459717@qq.com>
2024-08-11 10:14:57 +0800

    初步实现

--- blog-server/src/main/java/com/blog/controller/ArticleController.java
+++ blog-server/src/main/java/com/blog/controller/ArticleController.java
@@ -1,11 +1,29 @@
 package com.blog.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.blog.result.Result;
+import com.blog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+    /**
+     * 查询所有文章
+     * @return
+     */
+    @GetMapping("load")
+    public Result load(){
+        Result result =  articleService.load();
+        return result;
+    }
+    @GetMapping("selectByUserId/{id}")
+    public Result selectByUserId(@PathVariable Long id){
+        Result result = articleService.selectById(id);
+        return result;
+    }
 
 
 
