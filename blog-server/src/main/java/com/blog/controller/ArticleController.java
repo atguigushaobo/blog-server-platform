5e030e2bf6ad3f9c4c137268ad8b46cd72d9c20a
wolawola <3513459717@qq.com>
2024-08-12 18:25:56 +0800

    基本逻辑的初步实现

--- blog-server/src/main/java/com/blog/controller/ArticleController.java
+++ blog-server/src/main/java/com/blog/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package com.blog.controller;
 
+import com.blog.pojo.SaveArticle;
+import com.blog.pojo.SelectArticle;
 import com.blog.result.Result;
 import com.blog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +26,30 @@ public class ArticleController {
         Result result = articleService.selectById(id);
         return result;
     }
-
-
+    @PostMapping("saveArticle")
+    public Result saveArticle(@RequestBody SaveArticle saveArticle,@RequestHeader String token){
+        Result result = articleService.saveArticle(saveArticle,token);
+        return  result;
+    }
+    @GetMapping("selectByArticleId/{id}")
+    public Result selectByArticleId(@PathVariable Long id){
+        Result result = articleService.selectByArticleId(id);
+        return result;
+    }
+    @GetMapping("articleDetails/{id}")
+    public Result articleDetails(@PathVariable Long id){
+        Result result = articleService.articleDetails(id);
+        return result;
+    }
+    @DeleteMapping("deleteArticle/{id}")
+    public Result deleteArticle(@PathVariable Long id){
+        Result result = articleService.deleteArticle(id);
+        return result;
+    }
+    @PutMapping("updateArticle")
+    public Result updateArticle(@RequestBody SelectArticle selectArticle){
+        Result result = articleService.updateArticle(selectArticle);
+        return result;
+    }
 
 }
