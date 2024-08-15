310ae9cfafc76e4f13ca2d06d52653c535805ff9
1335625798 <1335625798@qq.com>
2024-08-15 22:12:15 +0800

    fix(Comment.java): 新增了jsonignore和jsonproperty注解过滤不需要的属性

--- blog-pojo/src/main/java/com/blog/entity/Comment.java
+++ blog-pojo/src/main/java/com/blog/entity/Comment.java
@@ -1,10 +1,14 @@
 package com.blog.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -13,10 +17,21 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 public class Comment implements Serializable {
+    @JsonIgnore
     private Long commentId;//自增主键
     private String commentBody;
     private Date createTime;
     private Long userId;
     private String username;
+    @JsonIgnore
     private Long articleId;//可能在联查的时候需要
+
+    @JsonIgnore
+    public Long getArticleId() {
+        return articleId;
+    }
+    @JsonProperty
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
 }
