d7a4f06ae7082f7af3c5397afeb8128052f497a4
1335625798 <1335625798@qq.com>
2024-08-15 22:13:36 +0800

    feat(BehaviourController.java): controller层代码

--- blog-server/src/main/java/com/blog/controller/BehaviourController.java
+++ blog-server/src/main/java/com/blog/controller/BehaviourController.java
@@ -1,7 +1,16 @@
 package com.blog.controller;
+import com.blog.entity.*;
+import com.blog.entity.Behavior;
+import com.blog.result.Result;
+import com.blog.service.BehaviorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 用户行为的模块
@@ -9,5 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/behaviour")
 public class BehaviourController {
+    @Autowired
+    private BehaviorService behaviorService;
+    @PostMapping("/likeOperate")
+    public Result likeOperate(@RequestBody Behavior behavior){
 
+        behaviorService.likeOperate(behavior);
+        return Result.success(null);
+    }
+    @GetMapping("/listComment/{articleId}")
+    @ResponseBody
+    public Result queryComment(@PathVariable Long articleId){
+        List<Comment> comments = behaviorService.queryComment(articleId);
+        return Result.success(comments);
+    }
+    @PostMapping("/addComment")
+    public Result addComment(@RequestBody Comment comment){
+        behaviorService.addComment(comment);
+        return Result.success(null);
+    }
+    @DeleteMapping("/deleteComment/{commentId}")
+    public Result delComment(@PathVariable Long commentId){
+        behaviorService.delComment(commentId);
+        return Result.success(null);
+    }
 }
