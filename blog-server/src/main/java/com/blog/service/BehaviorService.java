91fbdd30e6ffaac365a2c89a137476b02491db12
1335625798 <1335625798@qq.com>
2024-08-16 10:48:51 +0800

    feat(BehaviorService.xml)

---
+++ blog-server/src/main/java/com/blog/service/BehaviorService.java
@@ -0,0 +1,24 @@
+package com.blog.service;
+import com.blog.entity.*;
+
+import java.util.List;
+
+public interface BehaviorService {
+    /**
+     * 点赞，取消点赞
+     */
+    void likeOperate(Behavior behavior);
+    /**
+     * 查询所有评论
+     */
+    List<Comment> queryComment(Long articleId);
+    /**
+    * 新增评论
+    */
+    void addComment(Comment comment);
+    /**
+    * 删除评论
+     */
+    void delComment(long commentId);
+
+}
